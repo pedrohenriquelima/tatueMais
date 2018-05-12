f5ea2d1d0a4d2c22dd6dbffcaeb5e69b720eb444
Treinamento <treinamento@Treinamento1-Maquina222-83.local>
2018-05-12 16:41:30 -0300

    adicionado Firebase e CellView Personalizada

---
+++ tatueMais/TattooerTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TattooerTableViewCell.swift
+//  tatueMais
+//
+//  Created by Treinamento on 12/05/18.
+//  Copyright © 2018 Treinamento. All rights reserved.
+//
+
+import UIKit
+
+class TattooerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var tattooerCell: UIView!
+    @IBOutlet weak var tattooerCellImage: UIImageView!
+    @IBOutlet weak var tattoerCellName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
