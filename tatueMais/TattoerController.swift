34117b942de79e7a1f70f86eade306227cb9c6d1
pedrohenriquelima <pedrolimaesilva95@gmail.com>
2018-06-02 17:07:38 -0300

    Comunicação com o Firebase finalizada

--- tatueMais/TattoerController.swift
+++ tatueMais/TattoerController.swift
@@ -7,20 +7,41 @@
 //
 
 import UIKit
-import Firebase
+import FirebaseDatabase
 
 class TattoerController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var fireBaseReference: DatabaseReference?
+    var fireBaseDataHandle: DatabaseHandle?
+    
     var tattooerList = [Tattooer]()
+    let searchController = UISearchController(searchResultsController: nil)
+    
     @IBOutlet weak var tableViewTattooerList: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let tattooer1 = Tattooer(name: "Tattooer1", instagram: "@tattooer1", description: "Lorem ipsum interdum fringilla sodales quam elit dictum suscipit sociosqu curae quis, curabitur ut nostra turpis suscipit quam sit hac ut. ultricies tempor nisi per sollicitudin arcu nisl habitasse curae phasellus, cubilia turpis vivamus ultricies donec suscipit facilisis. ultricies suspendisse fringilla vulputate mauris aenean justo eleifend orci praesent, litora eros purus sociosqu maecenas molestie mi. tellus quis cursus felis sollicitudin hendrerit et habitasse, scelerisque ad tempus semper nibh etiam consequat sem, interdum ut ad consequat porttitor aliquam. lacinia lacus amet ullamcorper adipiscing amet praesent litora tristique, nibh imperdiet aliquet fringilla interdum fames aenean.", photoName: "022-man-4")
-        let tattooer2 = Tattooer(name: "Tattooer2", instagram: "@tattooer2", description: "Tincidunt mollis fames nisi metus consectetur vivamus sapien enim mollis, porttitor conubia molestie ornare viverra tempor vulputate himenaeos magna, lobortis donec fames aliquam elementum semper ultrices velit. inceptos nec iaculis lacinia nibh diam nec imperdiet fringilla orci eu, aliquam conubia pretium accumsan conubia eu auctor ultrices ipsum fermentum at, malesuada aliquet magna convallis augue curabitur semper quam eget. massa dictumst metus sem sed vulputate odio nisl dictum facilisis curabitur, gravida massa et convallis malesuada laoreet feugiat ante sociosqu non, fringilla quisque vel volutpat sem litora facilisis vivamus libero. facilisis aenean diam sociosqu sem convallis porttitor himenaeos porttitor maecenas, torquent orci pulvinar fames sem ad accumsan orci volutpat massa, consequat fermentum curabitur sem blandit rhoncus nostra justo.", photoName: "021-man-5")
-        let tattooer3 = Tattooer(name: "Tattooer3", instagram: "@tattooer3", description: "Eget arcu justo metus morbi tincidunt curae sem vestibulum volutpat senectus duis rutrum auctor, mauris pharetra mattis cubilia elit dolor quisque metus malesuada et litora est. mauris platea erat lacus ante mollis velit, quam tincidunt volutpat malesuada duis malesuada, auctor volutpat ipsum egestas pellentesque. placerat sodales nulla sodales nullam etiam adipiscing tellus imperdiet, vulputate rhoncus rutrum faucibus libero tortor urna ad odio, gravida vivamus eget lobortis tempus elementum ad. ultrices id iaculis elementum purus non lacinia dui imperdiet lobortis luctus lobortis, id dictum ad nunc urna semper per sociosqu leo interdum, sociosqu lacus nisi sit odio tristique torquent sem a tellus.", photoName: "020-man-6")
-        let tattooer4 = Tattooer(name: "Tattooer4", instagram: "@tattooer3", description: "Himenaeos convallis ante mauris est sed vehicula enim varius, ultricies aliquam lorem sed aliquam eget tempor curae nullam, tellus justo nibh iaculis amet ante consectetur. sagittis tincidunt pharetra enim sociosqu torquent proin risus aptent augue, himenaeos fermentum vestibulum maecenas quis iaculis tempor. pharetra venenatis congue dolor bibendum habitasse risus hac viverra ultricies diam, vel himenaeos ad curae fusce varius enim conubia cubilia. viverra curae adipiscing pellentesque suscipit id ad fusce, erat venenatis eu facilisis varius euismod, potenti praesent feugiat rhoncus fringilla nullam. lectus odio aenean auctor accumsan arcu vehicula class integer hac, convallis leo at morbi elit imperdiet lacinia.")
-        tattooerList.append(contentsOf:[tattooer1, tattooer2, tattooer3, tattooer4])
+
+        let tattooersRef = Database.database().reference(withPath: "tattooers")
+
+        tattooersRef.observe(.value, with: { snapshot in
+            var newTattooers: [Tattooer] = []
+            for child in snapshot.children {
+                if let snapshot = child as? DataSnapshot,
+                    let tattooer = Tattooer(snapshot: snapshot) {
+                    newTattooers.append(tattooer)
+                }
+            }
+            self.tattooerList = newTattooers
+            self.tableViewTattooerList.reloadData()
+        })
+        
+//        let tattooer1 = Tattooer(name: "Tattooer1", instagram: "@tattooer1", description: "Lorem ipsum interdum fringilla sodales quam elit dictum suscipit sociosqu curae quis, curabitur ut nostra turpis suscipit quam sit hac ut. ultricies tempor nisi per sollicitudin arcu nisl habitasse curae phasellus, cubilia turpis vivamus ultricies donec suscipit facilisis. ultricies suspendisse fringilla vulputate mauris aenean justo eleifend orci praesent, litora eros purus sociosqu maecenas molestie mi. tellus quis cursus felis sollicitudin hendrerit et habitasse, scelerisque ad tempus semper nibh etiam consequat sem, interdum ut ad consequat porttitor aliquam. lacinia lacus amet ullamcorper adipiscing amet praesent litora tristique, nibh imperdiet aliquet fringilla interdum fames aenean.", photoName: "022-man-4")
+//        let tattooer2 = Tattooer(name: "Tattooer2", instagram: "@tattooer2", description: "Tincidunt mollis fames nisi metus consectetur vivamus sapien enim mollis, porttitor conubia molestie ornare viverra tempor vulputate himenaeos magna, lobortis donec fames aliquam elementum semper ultrices velit. inceptos nec iaculis lacinia nibh diam nec imperdiet fringilla orci eu, aliquam conubia pretium accumsan conubia eu auctor ultrices ipsum fermentum at, malesuada aliquet magna convallis augue curabitur semper quam eget. massa dictumst metus sem sed vulputate odio nisl dictum facilisis curabitur, gravida massa et convallis malesuada laoreet feugiat ante sociosqu non, fringilla quisque vel volutpat sem litora facilisis vivamus libero. facilisis aenean diam sociosqu sem convallis porttitor himenaeos porttitor maecenas, torquent orci pulvinar fames sem ad accumsan orci volutpat massa, consequat fermentum curabitur sem blandit rhoncus nostra justo.", photoName: "021-man-5")
+//        let tattooer3 = Tattooer(name: "Tattooer3", instagram: "@tattooer3", description: "Eget arcu justo metus morbi tincidunt curae sem vestibulum volutpat senectus duis rutrum auctor, mauris pharetra mattis cubilia elit dolor quisque metus malesuada et litora est. mauris platea erat lacus ante mollis velit, quam tincidunt volutpat malesuada duis malesuada, auctor volutpat ipsum egestas pellentesque. placerat sodales nulla sodales nullam etiam adipiscing tellus imperdiet, vulputate rhoncus rutrum faucibus libero tortor urna ad odio, gravida vivamus eget lobortis tempus elementum ad. ultrices id iaculis elementum purus non lacinia dui imperdiet lobortis luctus lobortis, id dictum ad nunc urna semper per sociosqu leo interdum, sociosqu lacus nisi sit odio tristique torquent sem a tellus.", photoName: "020-man-6")
+//        let tattooer4 = Tattooer(name: "Tattooer4", instagram: "@tattooer3", description: "Himenaeos convallis ante mauris est sed vehicula enim varius, ultricies aliquam lorem sed aliquam eget tempor curae nullam, tellus justo nibh iaculis amet ante consectetur. sagittis tincidunt pharetra enim sociosqu torquent proin risus aptent augue, himenaeos fermentum vestibulum maecenas quis iaculis tempor. pharetra venenatis congue dolor bibendum habitasse risus hac viverra ultricies diam, vel himenaeos ad curae fusce varius enim conubia cubilia. viverra curae adipiscing pellentesque suscipit id ad fusce, erat venenatis eu facilisis varius euismod, potenti praesent feugiat rhoncus fringilla nullam. lectus odio aenean auctor accumsan arcu vehicula class integer hac, convallis leo at morbi elit imperdiet lacinia.")
+//        tattooerList.append(contentsOf:[tattooer1, tattooer2, tattooer3, tattooer4])
         
         tableViewTattooerList.delegate = self
         tableViewTattooerList.dataSource = self
--- tatueMais/Tattooer.swift
+++ tatueMais/Tattooer.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class Tattooer {
     var name: String
@@ -22,11 +23,47 @@ class Tattooer {
         self.photoName = "profileblur"
         self.photo = UIImage(named: "profileblur")!
     }
+    
     init(name: String, instagram: String, description: String, photoName: String) {
         self.name = name
         self.instagram = instagram
         self.description = description
         self.photoName = photoName
+        if let photo = UIImage(named: self.photoName) {
+            self.photo = photo
+        } else {
+            self.photo = UIImage(named: "profileblur")!
+        }
+        
+    }
+    
+    init?(snapshot: DataSnapshot) {
+        guard
+            let value = snapshot.value as? [String: AnyObject],
+            let name = value["name"] as? String,
+            let instagram = value["instagram"] as? String,
+            let description = value["description"] as? String
+            else {
+                return nil
+        }
+        self.name = name
+        self.instagram = instagram
+        self.description = description
+        if let photoName = value["profilePhoto"] as? String{
+            self.photoName = photoName
+        } else {
+            self.photoName = "profileblur"
+        }
         self.photo = UIImage(named: self.photoName)!
+    }
+    
+    func toAnyObject() -> Any {
+        return [
+            "name": name,
+            "instagram": instagram,
+            "description": description,
+            "photoName" : photoName,
+            "photo" : photo
+       ]
     }
 }
